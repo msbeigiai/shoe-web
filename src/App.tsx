480e543dceb56bf0a543296dcd0a3f1718557c8d
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-03-22 00:21:31 +0200

    Create a Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import '@fontsource-variable/nunito-sans';
 import Nav from "./components/Nav";
 import ShoeDetails from "./components/ShoeDetails";
+import Card from './components/Card';
+import { SHOE_LIST } from './Constant';
 
 function App() {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      <Nav />
-      <ShoeDetails />
+      {/* <Nav />
+      <ShoeDetails /> */}
+      <Card item={SHOE_LIST[0]} />
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+import { ItemType } from "../Constant"
+
+interface Props {
+  item: ItemType
+}
+
+
+const Card = ({ item }: Props) => {
+  return (
+    <div className={`${item.className} max-w-xl transform transition hover:scale-105 cursor-pointer`}>
+      <div className="p-8">
+        <div className="text-2xl font-bold">
+          {item.title}
+        </div>
+        <div className="underline underline-offset-4 font-semibold mt-10">SHOP NOW +</div>
+      </div>
+      <img className="absolute h-40 w-56 top-5 left-[40%]" src={item.src} />
+    </div>
+  )
+}
+
+export default Card
