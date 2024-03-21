ec67777a3d91336646e4584c4c7ddb0aa691cc64
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-03-21 22:49:59 +0200

    Add check boxes

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,7 +1,23 @@
+import { IoIosArrowDown } from "react-icons/io";
+interface Props {
+  title: string;
+  options: number[];
+}
 
-const Select = () => {
+const Select = ({ title, options }: Props) => {
+
   return (
-    <div>Select</div>
+    <div className="relative">
+      <select defaultValue={""} className="w-24 appearance-none border bg-white border-gray-300 p-4">
+        <option value="" disabled hidden>
+          {title}
+        </option>
+        {options.map((option) => <option value={option} key={option}>{option}</option>)}
+      </select>
+      <div className="absolute inset-y-0 right-0 flex-center pr-3">
+        <IoIosArrowDown />
+      </div>
+    </div>
   )
 }
 
