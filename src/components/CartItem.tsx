d6f6fe44b0753ae0d2ab28a94bac747ffbe1f09f
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-03-22 17:17:41 +0200

    Add elements to shopping card drawer

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,17 +1,41 @@
-import { ItemType } from "../Constant"
+import { CiTrash } from "react-icons/ci";
+import { ItemType, QTY, SIZES } from "../Constant"
+import Select from "./Select";
 
 interface Props {
   item: ItemType;
 }
+
 const CartItem = ({ item }: Props) => {
   return (
-    <div className="flex hover:bg-[#DAFFA2] p-2 cursor-pointer bg-gray-50 space-x-2">
-      <img className="h-24" src={item.src} />
-      <div className="space-y-2">
-        <div className="font-bold">{item.title}</div>
-        <div className="text-sm text-gray-400">{item.description}</div>
+    <div className="cursor-pointer p-2 hover:bg-[#DAFFA2] bg-gray-50 space-y-2">
+      {/* Image */}
+      <div className="flex space-x-2">
+        <img className="h-24" src={item.src} />
+        <div className="space-y-2">
+          {/* Title & Description */}
+          <div className="font-bold">{item.title}</div>
+          <div className="text-sm text-gray-400">{item.description}</div>
+        </div>
+        {/* Price */}
+        <div className="font-bold">{item.price}$</div>
       </div>
-      <div className="font-bold">{item.price}$</div>
+
+      <div className="flex justify-between pl-32">
+        <div className="flex space-x-6">
+          <div>
+            <div className="font-bold">SIZE</div>
+            <Select title="" options={SIZES} />
+          </div>
+          <div>
+            <div className="font-bold">QTY</div>
+            <Select title="" options={QTY} />
+          </div>
+        </div>
+        <button>
+          <CiTrash size={25} className="text-black" />
+        </button>
+      </div>
     </div>
   )
 }
